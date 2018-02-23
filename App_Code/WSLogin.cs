6045f53ab13deee9533d108226a413c316a5cbef
MuaRemus <luisnaruto.17@gmail.com>
2018-02-22 22:49:46 -0600

    Inicios

---
+++ App_Code/WSLogin.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Descripción breve de WSLogin
+/// </summary>
+[WebService(Namespace = "http://localhost/WSIntroduccion/WSIntroduccion")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+[System.Web.Script.Services.ScriptService]
+public class WSLogin : System.Web.Services.WebService
+{
+
+    public WSLogin()
+    {
+
+        //Elimine la marca de comentario de la línea siguiente si utiliza los componentes diseñados 
+        //InitializeComponent(); 
+    }
+
+    //[WebMethod]
+    //public string HelloWorld()
+    //{
+    //    return "Hola a todos";
+    //}
+    [WebMethod(EnableSession = true)]
+    public User Login(User user)
+    {
+        // Aqui iria la logica de base de datos//
+        if (user.Nick == "luis" && user.Password == "123")
+        {
+            User _user = new global::User()
+            {
+                Id = 1001,
+                Name = "Ramón Gerardo",
+                LastName = "Atilano",
+            };
+            //Crear sesion con el id del usuario
+            HttpContext.Current.Session["Identificador"] = _user.Id;
+            return _user;
+        }
+        else
+            throw new Exception("User not found!!! = (");
+
+    }
+}
---
+++ Scripts/Js/main.js
@@ -0,0 +1,62 @@
+﻿$(document).ready(function () {
+    //$('#btnPrueba').click(function () {
+    //    funcionWebService();
+    //});
+    $('#btnLogin').click(function () {
+        var nick = $('#txtNick').val();
+        var password = $('#txtPassword').val();
+        if (nick != '' && password != '') {
+            MiFuncionLogin(nick, password);
+        }
+        else {
+            alert('Proporcione Nick y Password')
+        }
+    });
+
+});
+function funcionWebService() {
+    $.ajax({
+        url: "WSLogin.asmx/HelloWorld",
+        data: "",
+        dataType: "json",
+        type: "POST",
+        contentType: "application/json; utf-8",
+        success: function (msg) {
+            alert(msg.d.name);
+        },
+        error: function (result) {
+            alert("ERROR " + result.status + ' ' + result.statusText);
+        }
+
+    });
+
+
+};
+function MiFuncionLogin(nick, password) {
+    var objJson = {
+        "Nick": nick,
+        "Password": password
+    };
+    var stringJson = JSON.stringify(objJson);
+    $.ajax({
+
+        url: "WSLogin.asmx/Login",
+        data: "{'user':" + stringJson + "}",
+        dataType: "json",
+        type: "POST",
+        contentType: "application/json; utf-8",
+        success: function (msg) {
+            if (msg.d.Id > 0) {
+                window.location.href = "Bienvenido.aspx"
+            }
+            else {
+                alert('No existe usuario!!!');
+                //alert(msg.d.Name)
+            }
+        },
+        error: function (result) {
+            alert("ERROR " + result.status + ' ' + result.statusText);
+        }
+
+    });
+}
